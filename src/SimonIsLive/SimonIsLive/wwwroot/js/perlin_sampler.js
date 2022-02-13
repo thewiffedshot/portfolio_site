f9c7f015c7b18167701ec6efd24eb99e0de2c999
Simeon Laplev <thewiffedshot@gmail.com>
2022-02-13 16:10:49 +0200

    Commit.

---
+++ src/SimonIsLive/SimonIsLive/wwwroot/js/perlin_sampler.js
@@ -0,0 +1,89 @@
+﻿class PerlinSampler {
+    canvas;
+    pngSampleUrl;
+    sampleWidth;
+    sampleHeight;
+    sampleColorData;
+    sampleNormalizedIntensities;
+    onsampleready;
+
+    constructor(pngSampleUrl) {
+        this.pngSampleUrl = pngSampleUrl;
+        this.canvas = document.createElement('canvas');
+        this.#loadSample(pngSampleUrl);
+    }
+
+    sampleRandomColumn() {
+        return this.sampleNormalizedIntensities[this.#getRandomInt(this.sampleWidth)].slice();
+    }
+
+    #getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+    }
+
+    #normalizeData() {
+        this.sampleNormalizedIntensities = Array(this.sampleWidth).fill(0).map(x => Array(this.sampleHeight).fill(0));
+
+        for (var i = 0, y = 0; i < this.sampleColorData.data.length; y++) {
+            if (y === this.sampleHeight - 1)
+                y = 0;
+
+            for (var x = 0; x < this.sampleWidth; i += 4, x++) {
+                var value = this.sampleColorData.data[i] / 255;
+
+                this.sampleNormalizedIntensities[x][y] = value;
+            }
+        }
+
+        this.onsampleready();
+    }
+
+    #loadSample(url) {
+        var _this = this;
+
+        var request = new XMLHttpRequest();
+
+        request.responseType = 'blob';
+
+        request.open('GET', url, true);
+
+        request.onload = function (e) {
+            if (request.readyState === 4) {
+                if (request.status === 200) {
+                    var img = new Image();
+                    var response = request.response;
+
+                    var reader = new FileReader();
+
+                    reader.onloadend = function () {
+                        img.src = reader.result;
+
+                        img.onload = function () {
+                            var context = _this.canvas.getContext('2d');
+
+                            _this.sampleWidth = Math.max(1, Math.floor(img.naturalWidth));
+                            _this.sampleHeight = Math.max(1, Math.floor(img.naturalHeight));
+
+                            _this.canvas.width = _this.sampleWidth;
+                            _this.canvas.height = _this.sampleHeight;
+
+                            context.drawImage(img, 0, 0);
+                          
+                            _this.sampleColorData = context.getImageData(0, 0, _this.sampleWidth, _this.sampleHeight);
+
+                            _this.#normalizeData();
+                        }
+                    }
+
+                    reader.readAsDataURL(response);           
+                }
+            }
+        }
+
+        request.onerror = function (e) {
+            console.error(request.statusText);
+        }
+
+        request.send(null);
+    }
+}
---
+++ src/SimonIsLive/SimonIsLive/wwwroot/js/site.js
@@ -0,0 +1,108 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+document.addEventListener("mousemove", parallax);
+document.addEventListener("touchmove", parallax);
+$('document').ready(perlinFloat);
+
+function parallax(e) {
+    this.querySelectorAll('.parallax').forEach(layer => {
+        lastMove = Date.now();
+        const speed = layer.getAttribute('data-speed');
+
+        var pageX;
+        var pageY;
+
+        if ((e.pageX) && (e.pageY)) {
+            pageX = e.pageX;
+            pageY = e.pageY;
+        } else if (e.targetTouches) {
+            pageX = e.targetTouches[0].clientX;
+            pageY = e.targetTouches[0].clientY;
+            e.preventDefault();
+        }
+
+        const x = (window.innerWidth - pageX * speed) / 333;
+        const y = (window.innerHeight - pageY * speed) / 333;
+
+        $(layer).data('parallax-x-offset', x);
+        $(layer).data('parallax-y-offset', y);
+    });
+}
+
+class PerlinFloatObject {
+    #element;
+    #amplitude;
+    #samplePerlinX;
+    #samplePerlinY;
+    #position;
+    #indexX = 0;
+    #indexY = 0;
+    #indexIncrementX = 1;
+    #indexIncrementY = 1;
+
+    constructor(element, amplitude, samplePerlinX, samplePerlinY) {
+        this.#element = element;
+        this.#amplitude = amplitude;
+        this.#samplePerlinX = samplePerlinX;
+        this.#samplePerlinY = samplePerlinY;
+        this.#position = { x: 0, y: 0 };
+
+        $(element).data('parallax-x-offset', 0);
+        $(element).data('parallax-y-offset', 0);
+    }
+
+    lin_interp(x, a, b) {
+        return a + x * (b - a);
+    }
+
+    move(delta) {
+        if (this.#indexX === this.#samplePerlinX.length || this.#indexX < 0) {
+            this.#indexIncrementX *= -1;
+        }
+
+        if (this.#indexY === this.#samplePerlinY.length || this.#indexY < 0) {
+            this.#indexIncrementY *= -1;
+        }
+
+        this.#position.x = this.lin_interp((this.#samplePerlinX[this.#indexX] - 0.5) * 2, -this.#amplitude, this.#amplitude);
+        this.#position.y = this.lin_interp((this.#samplePerlinY[this.#indexY] - 0.5) * 2, -this.#amplitude, this.#amplitude);
+
+        this.#element[0].style.transform = `translateX(${this.#position.x + this.#element.data('parallax-x-offset')}px) translateY(${this.#position.y + this.#element.data('parallax-y-offset')}px)`;
+
+        this.#indexX += this.#indexIncrementX;
+        this.#indexY += this.#indexIncrementY;
+    }
+}
+
+function perlinFloat() {
+    var timestepMs = 7;
+    var floatObjects = [];
+    var perlinSampler = new PerlinSampler('/images/perlin_sample.png');
+    var lastTime = Date.now();
+
+    perlinSampler.onsampleready = function () {
+        $(".float").each(function () {
+            floatObjects.push(
+                new PerlinFloatObject(
+                    $(this),
+                    Math.max($(this).data('float-minamp'), Math.random() * $(this).data('float-maxamp')),
+                    perlinSampler.sampleRandomColumn(),
+                    perlinSampler.sampleRandomColumn()
+                ));
+        });
+
+        let id = null;
+
+        clearInterval(id);
+
+        id = setInterval(frame, timestepMs);
+
+        function frame() {
+            floatObjects.forEach(x => x.move((Date.now() - lastTime) / 1000));
+            lastTime = Date.now();
+        }
+    };
+}
